36a18a2395211e2af212106e58c84f7a32dc0e1b
Richard Joffray <rjoffray@richards-mbp.lan>
2020-01-24 11:50:50 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# app-multi-display
+
+This is an example of how to implement multiple windows for OakOS using a single display
+
+## Running locally
+
+Make sure that you are running the right version of Node locally. You will find the required version in the `.nvmrc` file
+If you are not running the same version (`node -v`) then you will need to run
+
+``` bash
+nvm install $(cat .nvmrc)
+npm run rebuild
+```
+
+### Now you can run electron locally
+
+``` bash
+npm run dev
+```
+
+### Running in a docker container
+
+``` bash
+xhost +
+docker-compose up --build
+```
+
+### Shutting down the  docker container
+
+``` bash
+docker-compose down
+```
+
+## Example Installation
+
+``` json
+{
+  "services": [
+    {
+      "image": "index.docker.io/oaklabs/app-multi-window:latest",
+      "environment": {
+        "TZ": "America/Phoenix",
+        "REMOTE_URL":"https://www.fast.com",
+        "WINDOW_PERCENT":".5",
+        "WINDOW_X": "0",
+        "BACKGROUND_COLOR":"#000000",
+        "WINDOW_ONTOP":"false",
+        "SSL_EXCEPTIONS":"localhost;*.google.com",
+        "WINDOW_INSECURE":"false",
+        "DISPLAY_ID":"0",
+        "ELECTRON_DISABLE_SECURITY_WARNINGS":"true",
+        "WAIT_ON":"https://www.fast.com;https://www.google.com"
+      }
+    }
+  ]
+}
+
+```
---
+++ src/index.js
@@ -0,0 +1,75 @@
+const oak = require('oak')
+const waitOn = require('wait-on');
+let dns = require('dns')
+
+require('dotenv').config()
+
+function loadWindow (position) {
+  
+      let exceptions = ['localhost']
+      let displays = oak.getDisplays()
+      let display = parseInt(process.env.DISPLAY_ID) || 0
+      let fullscreen = Boolean(process.env.FULLSCREEN) || false
+      let url = process.env.REMOTE_URL || 'https://zivelo.com/'
+      let width = displays[display].workArea.width
+      let height = displays[display].workArea.height
+      let percent = process.env.WINDOW_PERCENT || 1
+      let size = (width * percent)  + "x" + height
+      let x = width -  (width * percent)
+      if(process.env.WINDOW_X ) {
+        x = parseInt(process.env.WINDOW_X)
+      }
+      let background = process.env.BACKGROUND_COLOR || '#ffffff'
+      let ontop = Boolean(process.env.WINDOW_ONTOP) || false
+      let insecure = Boolean(process.env.WINDOW_INSECURE)  || false
+
+
+      console.log("Displays: ",JSON.stringify(displays))
+
+      let opts = {
+        url,
+        background,
+        display,
+        ontop,
+        size: size,
+        x: x,
+        y: displays[display].workArea.y,
+        sslExceptions:  exceptions,
+        insecure,
+        fullscreen
+      }
+      if (opts.fullscreen) {
+        delete opts.size
+      }
+
+      // let opts = process.env
+      if (process.env.SSL_EXCEPTIONS) {
+        opts.sslExceptions = process.env.SSL_EXCEPTIONS.split(';')
+      }
+
+      console.log("Options: ", opts)
+
+      oak.load(opts)
+
+      
+
+}
+
+// everything has to wait for the main ready event to fire
+oak.on('ready', () => {
+  let waitFor = [process.env.REMOTE_URL];
+  if (process.env.WAIT_ON) {
+    waitFor = process.env.WAIT_ON.split(";")
+  }
+  console.log("waitFor: ", waitFor)
+  let opts = {
+    resources: waitFor
+  }
+  waitOn(opts, function (err) {
+    if (err) { return handleError(err); }
+    // once here, all resources are available
+    loadWindow("one")
+    loadWindow("two")
+  });
+  
+})
